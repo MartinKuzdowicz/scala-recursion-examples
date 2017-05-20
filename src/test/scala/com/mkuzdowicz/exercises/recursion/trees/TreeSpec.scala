8fdb67af051458eb9a26af54bf515318d2a644ac
Kuzdowicz, Martin (Developer) <martin.kuzdowicz@sky.uk>
2017-05-20 21:12:26 +0100

    test

--- src/test/scala/com/mkuzdowicz/exercises/recursion/trees/TreeSpec.scala
+++ src/test/scala/com/mkuzdowicz/exercises/recursion/trees/TreeSpec.scala
@@ -6,8 +6,7 @@ import org.junit.Test
 class TreeSpec {
 
   @Test
-  def tree_sum_function_should_sum_all_nodes_values = {
-
+  def tree_sum_function_should_sum_all_nodes_values_1 = {
     val rootNode = Node(4)
     val node1 = Node(1)
     val node2 = Node(3)
@@ -15,18 +14,27 @@ class TreeSpec {
     val node4 = Node(2)
     val node5 = Node(10)
     val node6 = Node(89)
-
     rootNode.left = node1
     rootNode.right = node2
     rootNode.left.left = node3
     rootNode.left.right = node4
     rootNode.left.right.left = node5
     rootNode.left.right.right = node6
-
-    val tree = new Tree(rootNode);
+    val tree = new Tree(rootNode)
 
     assertEquals(116, tree.sum())
+  }
 
+  @Test
+  def tree_sum_function_should_sum_all_nodes_values_2 = {
+    val rootNode = Node(10)
+    val node1 = Node(11)
+    val node2 = Node(500)
+    rootNode.left = node1
+    rootNode.right = node2
+    val tree = new Tree(rootNode)
+
+    assertEquals(521, tree.sum())
   }
 
 }
