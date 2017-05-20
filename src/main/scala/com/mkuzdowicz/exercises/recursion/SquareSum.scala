645341be783808cd897a1db86f7da9715afa3f43
Kuzdowicz, Martin (Developer) <martin.kuzdowicz@sky.uk>
2017-05-20 23:31:43 +0100

    adding iterative impl

--- src/main/scala/com/mkuzdowicz/exercises/recursion/SquareSum.scala
+++ src/main/scala/com/mkuzdowicz/exercises/recursion/SquareSum.scala
@@ -4,4 +4,8 @@ object SquareSum {
 
   def squareSumRecursive(n: Int): Int = if (n < 2) 1 else n * n + squareSumRecursive(n - 1)
 
+  def squareSumIteratively(n: Int): Int = {
+    (1 to n).map(x => x * x).reduce((x, bucket) => x + bucket)
+  }
+
 }
--- src/test/scala/com/mkuzdowicz/exercises/recursion/SquareSumSpec.scala
+++ src/test/scala/com/mkuzdowicz/exercises/recursion/SquareSumSpec.scala
@@ -12,4 +12,11 @@ class SquareSumSpec {
     assertEquals(91, SquareSum.squareSumRecursive(6))
   }
 
+  @Test
+  def square_sum_iteratively_impl_returns_correct_values(): Unit = {
+    assertEquals(5, SquareSum.squareSumIteratively(2))
+    assertEquals(385, SquareSum.squareSumIteratively(10))
+    assertEquals(91, SquareSum.squareSumIteratively(6))
+  }
+
 }
