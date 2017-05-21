06e5bca77345d4458b4338a60b109c86582d73ff
Kuzdowicz, Martin (Developer) <martin.kuzdowicz@sky.uk>
2017-05-21 01:25:39 +0100

    more tests

--- src/test/scala/com/mkuzdowicz/exercises/recursion/FizzBuzzSpec.scala
+++ src/test/scala/com/mkuzdowicz/exercises/recursion/FizzBuzzSpec.scala
@@ -8,11 +8,16 @@ class FizzBuzzSpec {
   @Test
   def fizz_buzz_iteratively_return_correct_results(): Unit = {
     assertEquals((15, FizzBuzz.FIZZ_BUZZ), FizzBuzz.fizzBuzzIteratively(15).apply(0))
+    assertEquals((20, FizzBuzz.FIZZ), FizzBuzz.fizzBuzzIteratively(20).apply(0))
+    assertEquals((21, FizzBuzz.BUZZ), FizzBuzz.fizzBuzzIteratively(21).apply(0))
+
   }
 
   @Test
   def fizz_buzz_recursively_return_correct_results(): Unit = {
     assertEquals((15, FizzBuzz.FIZZ_BUZZ), FizzBuzz.fizzBuzzRecursively(15).apply(0))
+    assertEquals((20, FizzBuzz.FIZZ), FizzBuzz.fizzBuzzRecursively(20).apply(0))
+    assertEquals((21, FizzBuzz.BUZZ), FizzBuzz.fizzBuzzRecursively(21).apply(0))
   }
 
 }
