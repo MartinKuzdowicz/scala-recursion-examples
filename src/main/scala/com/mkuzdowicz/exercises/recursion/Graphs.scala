ca636a64f0d34761366caa5c79a4f16cd6e0c0e4
Kuzdowicz, Martin (Developer) <martin.kuzdowicz@sky.uk>
2017-05-21 16:48:32 +0100

    example graph

---
+++ src/main/scala/com/mkuzdowicz/exercises/recursion/Graphs.scala
@@ -0,0 +1,12 @@
+package com.mkuzdowicz.exercises.recursion
+
+object Graphs {
+
+  val graph = Array(
+    Array(0, 2, 3, 0),
+    Array(0, 0, 5, 4),
+    Array(0, 0, 0, 2),
+    Array(1, 0, 0, 0)
+  )
+
+}
